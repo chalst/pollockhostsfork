1e8ed178d69a3b60c03dec86ff576281364d99ea
Charles Stewart <stewart@textproof.com>
2013-02-06 17:43:33 +0100

    Reorganised do-update scripts to make them easier to debug and apply in parts

---
+++ do-check-shortlog.sh
@@ -0,0 +1,21 @@
+#source this file
+#Need binary: git (with checkout, shortlog)
+#Need: to set up local repo to have a "dist" branch that is not customised
+
+# test in branch 'dist' (like `git checkout dist`)
+
+git shortlog | grep -e "$pubdate" 
+
+xst=$?
+if test $xst = 0; then
+     echo "$pubdate logged already"
+     return 1
+elif test $xst -ge 2; then
+     echo "grep -e $pubdate fails with exit status $xst"
+     return $xst
+fi
+
+commitmsg = "fetch update of $pubdate from $hostsurl"
+echo "commitmsg=\"$commitmsg\""
+return 0
+
