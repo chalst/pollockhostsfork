d95a8b0967e2f370dcd63f209f23c707f21a3f4e
Charles Stewart <stewart@textproof.com>
2013-02-07 13:09:18 +0100

    fix assignment of commitmsg

--- do-check-shortlog.sh
+++ do-check-shortlog.sh
@@ -3,6 +3,7 @@
 #Need: to set up local repo to have a "dist" branch that is not customised
 
 # test in branch 'dist' (like `git checkout dist`)
+test -n "$pubdate" || return 1
 
 git shortlog | grep -e "$pubdate" 
 
@@ -15,7 +16,7 @@ elif test $xst -ge 2; then
      return $xst
 fi
 
-commitmsg = "fetch update of $pubdate from $hostsurl"
+commitmsg="fetch update of $pubdate from $hostsurl"
 echo "commitmsg=\"$commitmsg\""
 return 0
 
